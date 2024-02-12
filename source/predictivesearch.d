69c35eadd9c96c77e15f9216ba67d7be7fa656ba
phebert5009 <cpierrot273@gmail.com>
2024-02-12 00:58:20 -0800

    Resolve issues
    See previous commit for details on the issues.
    I may need to do some clean up.

--- source/predictivesearch.d
+++ source/predictivesearch.d
@@ -5,7 +5,7 @@
  * we can then use the first and last object in the range to try and predict where the item we
  * are looking for would be. This means that we would be doing less memory accesses.
  *
- * The formula for the prediction is `indexOf(min) + (max - needle)/(max - min) * (indexOf(max) - indexOf(min))`
+ * The formula for the prediction is `indexOf(min) + (needle - min)/(max - min) * (indexOf(max) - indexOf(min))`
  */
 module predictivesearch;
 
@@ -39,19 +39,24 @@ if(is(ReturnType!((R r, T t, size_t index) => r[index] == t) : bool) &&
 
    while(max != min)
    {
-      size_t prediction = cast(size_t)(minIndex + (max - needle).to!double / (max - min).to!double * (maxIndex - minIndex));
+      size_t prediction = cast(size_t)
+         (minIndex + (needle - min).to!double / (max - min).to!double * (maxIndex - minIndex));
+      auto actual = haystack[prediction];
+      if(actual == needle) return n_size_t(prediction);
+
       if(prediction == minIndex)
       {
          minIndex++;
          min = haystack[minIndex];
+         continue;
       }
       if(prediction == maxIndex) // should only happen if max < 0
       {
          maxIndex--;
          max = haystack[maxIndex];
+         continue;
       }
-      auto actual = haystack[prediction];
-      if(actual == needle) return n_size_t(prediction);
+
       if(actual < needle)
       {
          min = actual;
@@ -61,17 +66,22 @@ if(is(ReturnType!((R r, T t, size_t index) => r[index] == t) : bool) &&
       max = actual;
       maxIndex = prediction;
    }
+   if(min == needle) return n_size_t(minIndex);
    return n_size_t.init;
 }
 
 unittest
 {
-   assert(find([1,2,3,4], 1).get == 0);
+   auto res = find([1,2,3,4], 1);
+   assert(!res.isNull);
+   assert(res.get == 0);
 }
 
 unittest
 {
-   assert(find([1, 2, 3, 4], 4UL).get == 3);
+   auto res = find([1, 2, 3, 4], 4UL);
+   assert(!res.isNull);
+   assert(res.get == 3);
 }
 
 unittest
@@ -81,19 +91,21 @@ unittest
 
 unittest
 {
-   assert(find([4, 5, 41, 44, 45, 48, 60, 72, 76,
-            93, 102, 130, 132, 155, 174, 213, 230,
-            247, 258, 266, 266, 272, 282, 304, 315,
-            325, 334, 344, 350, 357, 369, 372, 376,
-            382, 386, 426, 429, 454, 466, 472, 476,
-            477, 478, 484, 508, 529, 539, 551, 571,
-            574, 575, 581, 596, 619, 624, 629, 634,
-            638, 653, 679, 680, 684, 684, 688, 698,
-            729, 731, 746, 761, 762, 769, 779, 791,
-            800, 806, 808, 818, 824, 828, 831, 833,
-            835, 846, 857, 864, 867, 873, 885, 898,
-            900, 918, 926, 936, 938, 941, 960, 977,
-            981, 986, 989], 155).get == 13);
+   auto res = find([4, 5, 41, 44, 45, 48, 60, 72, 76,
+                  93, 102, 130, 132, 155, 174, 213, 230,
+                  247, 258, 266, 266, 272, 282, 304, 315,
+                  325, 334, 344, 350, 357, 369, 372, 376,
+                  382, 386, 426, 429, 454, 466, 472, 476,
+                  477, 478, 484, 508, 529, 539, 551, 571,
+                  574, 575, 581, 596, 619, 624, 629, 634,
+                  638, 653, 679, 680, 684, 684, 688, 698,
+                  729, 731, 746, 761, 762, 769, 779, 791,
+                  800, 806, 808, 818, 824, 828, 831, 833,
+                  835, 846, 857, 864, 867, 873, 885, 898,
+                  900, 918, 926, 936, 938, 941, 960, 977,
+                  981, 986, 989], 155);
+   assert(!res.isNull);
+   assert(res.get == 13);
 }
 
 unittest
