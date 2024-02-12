c9789d8bfb5a35d35d0524bdeca415cb93ff8321
phebert5009 <cpierrot273@gmail.com>
2024-02-11 23:33:59 -0800

    Create the module

---
+++ source/predictivesearch.d
@@ -0,0 +1,16 @@
+/** Preditive Search
+ *
+ * A search algorithm based on Binary search which tries to reduce the number of memory accesses.
+ * If, on top of knowing that the input is sorted, we assume that it is distributed uniformally,
+ * we can then use the first and last object in the range to try and predict where the item we
+ * are looking for would be. This means that we would be doing less memory accesses
+ */
+module predictivesearch;
+
+size_t Find(R)(R randomAccessRange, T Needle)
+if(is(typeof((R r, size_t index) {
+        T t = r[index];
+    })) && is(typeof((R r) => R[$ - 1]))) // random access ranges have different requirements
+{
+   return 0; 
+}
